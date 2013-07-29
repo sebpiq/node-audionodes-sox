ae87a681b2bb31e6ddc0a1270252df8cb2e98c52
Sebastien Piquemal <sebpiq@gmail.com>
2013-07-29 13:22:04 +0400

    SoxEffect working + tests + added license

--- test/SoxEffect-test.js
+++ test/SoxEffect-test.js
@@ -1,59 +1,117 @@
 var assert = require('assert')
-  , async = require('async')
   , _ = require('underscore')
+  , async = require('async')
   , AudioBuffer = require('audiobuffer')
   , SoxEffect = require('../lib/SoxEffect')
 
 describe('SoxEffect', function() {
 
-  describe('pullAudio', function() {
+  describe('_tick', function() {
 
-    var helpers = require('./helpers')({approx: 0.00001})
+    var helpers = require('./helpers')({approx: 0.001})
 
     var dummyContext = {sampleRate: 44100, BLOCK_SIZE: 128}
 
-    it('should open a sound file and apply the effect on it', function(done) {
+    it('should open a mono file and apply the effect on it', function(done) {
       var soxEffect = new SoxEffect(dummyContext, __dirname + '/sounds/steps-16b-44khz-mono.wav', 'gain', 13.97847)
-        , buffers = []
-        , allAudio
-        , chArray
-      async.whilst(
-        function() { return soxEffect._returnCode === null },
-        function(next) {
-          soxEffect.pullAudio(function(err, buffer) {
-            assert.ok(!err)
-            assert.equal(buffer.length, 128)
-            assert.equal(buffer.numberOfChannels, 1)
-            assert.equal(buffer.sampleRate, 44100)
-            buffers.push(buffer)
-            next()
-          })
-        },
-        function() {
-          // Concatenate all the buffers in one 
-          allAudio = _.reduce(buffers, function(all, buffer) {
-            return all.concat(buffer)
-          }, new AudioBuffer(1, 0, 44100))
+        , blocks = [], block
+        , allAudio, sliced
+        , ended = false
 
+      soxEffect.on('ready', function() {
+        assert.deepEqual(soxEffect.format, {
+          numberOfChannels: 1,
+          bitDepth: 16,
+          sampleRate: 44100,
+          endianness: 'LE', signed: true
+        })
+
+        soxEffect.on('end', function() {
+          ended = true
+          // Concatenate all the blocks in one 
+          allAudio = _.reduce(blocks, function(all, block) {
+            return all.concat(block)
+          }, new AudioBuffer(1, 0, 44100))
           // Test that we got teh expected values
-          debugger
           _.range(21).forEach(function(i) {
-            chArray = allAudio.slice(i * 4410, (i + 1) * 4410)
-            helpers.assertAllValuesApprox(chArray, -1 + i * 0.3)
+            sliced = allAudio.slice(i * 4410, (i + 1) * 4410)
+            helpers.assertAllValuesApprox(sliced.getChannelData(0), -1 + i * 0.1)
           })
           done()
+        })
+
+        var audioTick = function() {
+          block = soxEffect._tick()
+          assert.equal(block.numberOfChannels, 1)
+          assert.equal(block.length, 128)
+          assert.equal(block.sampleRate, 44100)
+          blocks.push(block)
+          if (!ended) setTimeout(audioTick, 128/44100)
         }
-      )
+        audioTick()
+
+      })
+
     })
 
-    /*it('should return an error if the file doesn\'t exist', function(done) {
-      var soxEffect = new SoxEffect(__dirname + '/sounds/unknown.wav', 'gain', 13.97847)
-      soxEffect.pullAudio(function(err, buffer) {
+    it('should open a stereo file and apply the effect on it', function(done) {
+      var soxEffect = new SoxEffect(dummyContext, __dirname + '/sounds/steps-16b-44khz-stereo.wav', 'gain', 13.97847)
+        , blocks = [], block
+        , allAudio, sliced
+        , ended = false
+
+      soxEffect.on('ready', function() {
+        assert.deepEqual(soxEffect.format, {
+          numberOfChannels: 2,
+          bitDepth: 16,
+          sampleRate: 44100,
+          endianness: 'LE', signed: true
+        })
+
+        soxEffect.on('end', function() {
+          ended = true
+          // Concatenate all the blocks in one 
+          allAudio = _.reduce(blocks, function(all, block) {
+            return all.concat(block)
+          }, new AudioBuffer(2, 0, 44100))
+          // Test that we got teh expected values
+          _.range(21).forEach(function(i) {
+            sliced = allAudio.slice(i * 4410, (i + 1) * 4410)
+            helpers.assertAllValuesApprox(sliced.getChannelData(0), -1 + i * 0.1)
+            helpers.assertAllValuesApprox(sliced.getChannelData(1), 1 - i * 0.1)
+          })
+          done()
+        })
+
+        var audioTick = function() {
+          block = soxEffect._tick()
+          assert.equal(block.numberOfChannels, 2)
+          assert.equal(block.length, 128)
+          assert.equal(block.sampleRate, 44100)
+          blocks.push(block)
+          if (!ended) setTimeout(audioTick, 128/44100)
+        }
+        audioTick()
+
+      })
+
+    })
+
+    it('should return an error if the file doesn\'t exist', function(done) {
+      var soxEffect = new SoxEffect(dummyContext, '/sounds/unknown.wav', 'gain', 13.97847)
+      soxEffect.on('error', function(err) {
         assert.ok(err)
-        assert.ok(!buffer)
         done()
       })
-    })*/
+    })
+
+    it('should return an error if invalid arguments to sox', function(done) {
+      var soxEffect = new SoxEffect(dummyContext,  __dirname + '/sounds/steps-16b-44khz-stereo.wav', 'blablabla')
+      soxEffect.on('error', function(err) {
+        assert.ok(err)
+        done()
+      })
+    })
 
   })
 
